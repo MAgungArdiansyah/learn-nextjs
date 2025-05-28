89670a0787ed789c89c0b4457e77f24cc024753e
MAgungArdiansyah <55588249+MAgungArdiansyah@users.noreply.github.com>
2025-05-28 09:10:35 +0700

    add main_header

--- components/main_layout.tsx
+++ components/main_layout.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import MainHeader from './main_header'
 const MainLayout: React.FC<React.PropsWithChildren> = ({children}) => {
   return (
-    <div>
+    <div className='bg-gray-200 w-screen min-h-screen'>
+      <MainHeader />
       <h1>main_layout</h1>
       <hr />
       <main>{children}</main>
